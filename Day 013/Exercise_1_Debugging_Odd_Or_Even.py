de63498147acb7d80c3a757de219a24c84051a0e
Christine <163134814+cchristinechan@users.noreply.github.com>
2024-12-25 00:54:49 +0000

    Create Exercise_1_Debugging_Odd_Or_Even.py

---
+++ Day 013/Exercise_1_Debugging_Odd_Or_Even.py
@@ -0,0 +1,17 @@
+# Debugging Odd or Even
+# Read the code in exercise.py 
+# Spot the problems 🐞. 
+# Modify the code to fix the program. 
+# Fix the code so that it works and passes the tests when you submit.
+
+# def odd_or_even(number):
+#     if number % 2 = 0:
+#         return "This is an even number."
+#     else:
+#         return "This is an odd number."
+
+def odd_or_even(number):
+    if number % 2 == 0: # added another equals symbol
+        return "This is an even number."
+    else:
+        return "This is an odd number."
