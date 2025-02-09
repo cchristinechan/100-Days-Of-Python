393a55137559934502bed62d60771d5cb5d7d611
Christine <163134814+cchristinechan@users.noreply.github.com>
2025-02-09 20:35:57 +0000

    Add files via upload

---
+++ Day 034/ui.py
@@ -0,0 +1,77 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
+THEME_COLOR = "#375362"
+
+class QuizInterface:
+
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(column=1, row=0)
+
+        self.canvas = Canvas(width=300, height=250, bg="white", highlightthickness=0)
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            width=280,
+            text="blank",
+            fill=THEME_COLOR,
+            font=("Arial", 15, "italic"))
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
+
+        true_img = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_img, highlightthickness=0, bd=0, command=self.user_answer_true)
+        self.true_button.grid(row=2, column=0)
+
+        false_img = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_img, highlightthickness=0, bd=0, command=self.user_answer_false)
+        self.false_button.grid(row=2, column=1)
+
+        self.get_next_question()
+
+        self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")  # even if when you reach the end, you can reset the background colour
+        self.score_label.config(text=f"Score: {self.quiz.score}")
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.enable_buttons()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz!")
+            self.disable_buttons()
+
+
+    def user_answer_true(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+        self.disable_buttons()
+
+
+    def user_answer_false(self):
+        self.give_feedback(self.quiz.check_answer("False"))
+        self.disable_buttons()
+
+
+    def disable_buttons(self):
+        """Disabling the buttons to prevent users from spam clicking them"""
+        self.true_button.config(state="disabled")
+        self.false_button.config(state="disabled")
+
+
+    def enable_buttons(self):
+        self.true_button.config(state="active")
+        self.false_button.config(state="active")
+
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
