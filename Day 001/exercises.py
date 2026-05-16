48e0ba7a69c29aaf4f38228428230ae99a980121
cchristinechan <chanc7@tcd.ie>
2026-05-16 19:03:13 +0100

    Add exercises.py

---
+++ Day 001/exercises.py
@@ -0,0 +1,27 @@
+# exercise 1: printing practice
+print('''1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.
+2. Knead the dough for 10 minutes.
+3. Add 3g of Salt.
+4. Leave to rise for 2 hours.
+5. Bake at 200 degrees C for 30 minutes.''')
+
+# exercise 2: debugging practice
+# Fix the code below 👇
+
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print("New lines can be created with a \ and the letter n")
+
+# exercise 3: variables
+glass1 = "milk"
+glass2 = "juice"
+
+temp = glass1
+glass1 = glass2
+glass2 = temp
+
+
+# input
+print("Hello" + input("What is your name?" ))
